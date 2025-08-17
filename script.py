e4e7e10949c2fb6a5101c8bddd22a1ccc4405fad
Pavankalyan Magam <pavankalyanmagam@gmail.com>
2025-08-17 11:31:45 -0500

    "second commit"

---
+++ script.py
@@ -0,0 +1,115 @@
+# # Variables
+# print('********Variables*******')
+# price = 10
+# price = 20
+# rating  = 4.0
+# is_published = True
+# is_published = False
+# print(price)
+# print(rating)
+# print(is_published)
+#
+# # Hospital Exercise
+# print('*********Hospital Exercise*****')
+# full_name = 'John Smith'
+# age = 20
+# is_new_patient = True
+# print(full_name)
+# print(age)
+# print(is_new_patient)
+from idlelib.debugger_r import codetable
+
+## Receive Input From the User
+# print("Receive Input from the user")
+# name = input('What is your name? ')
+# color  = input('What is your favourite color? ')
+# print(name + ' like ' + color)
+
+# Calculate Age
+#
+# birth_year = input("Enter birth year: ")
+# print(type(birth_year))
+# age = 2025 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# Calculate Weight
+
+# weight_lbs = input("Weight (lbs): ")
+# weight_kg = float(weight_lbs) * 0.45
+# print(weight_kg)
+
+#Strings
+# course = "Python's for Beginners"
+# another = course[:]
+# print(course)
+# print(course[1])
+# print(course[-2])
+# print(course[0:])
+# print(course[:])
+# print(course[::-1])
+# print(course[1:3])
+# print(course[:5])
+# print(another)
+#
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# Formatted Strings
+# first = 'John'
+# last = 'Smith'
+#
+# message = first + ' [' + last + '] is a coder '
+# print(message)
+#
+#
+# msg = f'{first} [{last}] is a coder '
+# print(msg)
+
+## String Methods
+# course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course.lower())
+# print(course.find('r'))
+# print(course.replace('r','_'))
+# print(course.replace('Beginners','Absolute Beginners'))
+#
+# print('Python' in course)
+
+# # Arithmetic Operator
+#
+# print(10 + 2)
+# print(10 - 2)
+# print(10 * 2)
+# print(10 / 2)
+# print(10 // 2)
+# print(10 % 2)
+# print(10 ** 2)
+#
+# # Increment Operator
+# x = 10
+# x = x + 3
+# print(x)
+# y = 10
+# y += 3
+# print(y)
+
+# Operator Precedence
+# x = 10 + 3 * 2 ** 2
+# print(x)
+
+# Exponentiation 2 ** 3
+# multiplication or Division
+# addition or subtraction
+
+# Math Functions
+
+x = 2.9
+print(round(x))
+print(abs(-2.9))
+
+
+
+
+
